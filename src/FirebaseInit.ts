6b317050c6dbf2ef51a754ca939b3cf2b8a6a8af
DeFlesz <nieflesz@gmail.com>
2022-10-10 10:09:46 +0200

    added login functionality

---
+++ src/FirebaseInit.ts
@@ -0,0 +1,16 @@
+import {initializeApp} from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATwUYwyLkUEbCURkIHL5XxgTH8w1soCLk",
+  authDomain: "test1-ad935.firebaseapp.com",
+  projectId: "test1-ad935",
+  storageBucket: "test1-ad935.appspot.com",
+  messagingSenderId: "469258513789",
+  appId: "1:469258513789:web:350bf185127de3fae23014"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig)
