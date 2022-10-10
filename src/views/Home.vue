ed601e2d2f28ab611c48db5bdf0ae1f1e8a61d02
DeFlesz <nieflesz@gmail.com>
2022-10-10 23:49:03 +0200

    attempt at reading data (data was retrieved, but i cannot make it display)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,51 @@
 <template>
+  <!-- <div class="polls" ref="polls">
+    <div class="flex-polls">
+
+      <div class="" v-for="i in 10">
+        <Card
+            heading="Wybory na miss zsmeie"
+            creator-name="ZSMEIE TORUŃ"
+            :description="loremIpsum"
+            action-label="głosuj teraz"
+            @action="stuff"
+            class="item"
+            :img="image"
+        />
+      </div>
+    </div>
+    <div class="flex-polls">
+
+      <div class="" v-for="i in 10">
+        <Card
+            heading="Wybory na miss zsmeie"
+            creator-name="ZSMEIE TORUŃ"
+            :description="loremIpsum"
+            action-label="głosuj teraz"
+            @action="stuff"
+            class="item"
+            :img="image"
+        />
+      </div>
+    </div>
+
+  </div> -->
+
   <div class="polls" ref="polls">
-    <div class="flex-polls">
-
-      <div class="" v-for="i in 10">
-        <Card
-            heading="Wybory na miss zsmeie"
-            creator-name="ZSMEIE TORUŃ"
-            :description="loremIpsum"
-            action-label="głosuj teraz"
-            @action="stuff"
-            class="item"
-            :img="image"
-        />
-      </div>
+    <!-- <div class="flex-polls"> -->
+      <!-- render polls from testPolls Poll array -->
+    <div class="" v-for="poll in testPolls">
+      <Card
+        :heading="poll.name"
+        :creator-name="poll.creator"
+        :description="poll.description"
+        action-label="głosuj teraz"
+        @action="stuff(poll)"
+        class="item"
+        :img="image"
+      />
     </div>
-    <div class="flex-polls">
-
-      <div class="" v-for="i in 10">
-        <Card
-            heading="Wybory na miss zsmeie"
-            creator-name="ZSMEIE TORUŃ"
-            :description="loremIpsum"
-            action-label="głosuj teraz"
-            @action="stuff"
-            class="item"
-            :img="image"
-        />
-      </div>
-    </div>
-
+    <!-- </div> -->
   </div>
 
 </template>
@@ -38,6 +55,39 @@
 import Card from "@/components/Card.vue";
 import {onMounted, ref} from "vue";
 
+import app from "../FirebaseInit";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import type { Poll } from "@/types/poll";
+
+const db = getFirestore(app)
+
+// const dataFetched = ref<boolean>(false)
+//create Poll array
+const testPolls = ref<Array<Poll>>([])
+
+//retrieve data
+const querySnapshot = await getDocs(collection(db, "polls"))
+querySnapshot.forEach((doc) => {
+  let data = doc.data()
+  let tempPoll: Poll = {
+    name: data.name,
+    creator: data.creator,
+    description: data.description,
+    options: data.options,
+    expirationDate: data.expirationDate,
+    type: data.type,
+    active: data.active
+  }
+
+  console.log(tempPoll);
+  
+// push Poll into Poll array
+  testPolls.value.push(tempPoll)
+  console.log(testPolls);
+  
+})
+// dataFetched.value = true
+
 const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
 
 
@@ -46,8 +96,8 @@ const polls = ref<HTMLElement | null>(null)
 const image = "https://wio.waw.pl/static/files/gallery/8/1107884_1606493893.jpg"
 
 
-function stuff() {
-  console.log("kik")
+function stuff(poll: any) {
+  console.log(poll)
 }
 
 
