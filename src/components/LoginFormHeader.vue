ab4dfb9c47a31d7be357958af03c1e3be8871e92
hubertmalkowski <hubert.malkowski@gmail.com>
2022-10-20 22:17:27 +0200

    Login page heading added

--- src/components/LoginFormHeader.vue
+++ src/components/LoginFormHeader.vue
@@ -1,7 +1,8 @@
 <template>
 <section class="login-form-header">
   <div>
-
+    <h1>Elect.io</h1>
+    <span>Twój głos ma kluczowe znaczenie</span>
   </div>
 
 </section>
@@ -17,11 +18,47 @@
   .login-form-header {
     background-color: var(--Color-Primary-500);
 
+
   }
+  h1 {
+    font-size: 96px !important;
+    color: var(--Background) !important;
+
+  }
+  span {
+    display: initial !important;
+    font-size: 48px;
+    font-weight: 600;
+    line-height: 58px;
+    letter-spacing: 0em;
+    color: var(--Background);
+
+  }
+}
+
+span {
+  display: none;
+}
+
+
+h1 {
+  margin: 0;
+
+  color: var(--Color-Primary-500);
+  font-size: 40px;
+
 }
 .login-form-header {
   height: 100%;
   width: 100%;
+
+  display: flex;
+  padding-left: 14px;
+  flex-direction: column;
+  justify-content: center;
+  gap: 15px;
 }
+
+
 
 </style>
